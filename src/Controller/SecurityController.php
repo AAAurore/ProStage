c3aea692bc04635e92bc8994024e4f08524f88f2
BIADOS Aurore <abiados@iutbayonne.univ-pau.fr>
2021-03-22 18:02:52 +0100

    Création d'un formulaire d'inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +36,38 @@ class SecurityController extends AbstractController
     public function logout()
     {
         //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+    * @Route("/inscription", name="app_inscription")
+    */
+    public function inscription(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+      // Création d'unutilisateur vide
+      $utilisateur = new User();
+
+      // Création du formulaire permettant de saisir un utilisateur
+      $formulaireUtilisateur = $this ->createForm(UserType::class, $utilisateur);
+
+      /*On demande au formulaire d'analyser la dernière requête http. Si le tableau POST (car le formulaire est transmis par le protocole POST) contenu de cette requête contient des variables prenom, nom, email et password alors la méthode handleRequest() récupère les valeurs de ces variables et les affecte à l'objet $utilisateur */
+      $formulaireUtilisateur->handleRequest($request);
+
+      // Traiter les données du formulaire s'il a été soumis
+      if ($formulaireUtilisateur->isSubmitted() && $formulaireUtilisateur->isValid())
+      {
+        // Enregistrer l'utilisateur en BD
+        // $manager = $managerRegistry->getManager();
+        // $manager->persist($utilisateur);
+        // $manager->flush();
+
+        // Rediriger l'utilisateur vers la page d'accueil
+        return $this->redirectToRoute('pro_stage_accueil');
+      }
+
+      // Création de la représentation graphique du $vueFormulaireUtilisateur
+      $vueFormulaireUtilisateur = $formulaireUtilisateur->createView();
+
+      // Afficher la page présentant le formulaire d'inscription d'un utilisateur
+      return $this->render('security/inscription.html.twig', ['vueFormulaireUtilisateur' => $vueFormulaireUtilisateur]);
     }
 }
